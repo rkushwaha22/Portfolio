c15c270ce88ed44a0440b4de90f7b867becb6cd6
Rabindra kushwaha <rabindrakushwaha0001@gmail.com>
2026-04-02 08:59:42 +0530

    Update cloudinaryConfig.js

--- Backend/Middlewere/cloudinaryConfig.js
+++ Backend/Middlewere/cloudinaryConfig.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const CLOUD_NAME = process.env.CLOUD_NAME
-const API_KEY = process.env.API_KEY
-const API_SECRET = process.env.API_SECRET
-
 
 cloudinary.config({
-  cloud_name: CLOUD_NAME,         // cloud name
-  api_key: API_KEY,                //'aapka_api_key',
-  api_secret: API_SECRET          //'aapka_api_secret'
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
 });
 
 const storage = new CloudinaryStorage({
